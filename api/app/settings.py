5e5cb061681e12c34a1c2bf7dd53b4053de54691
bryan leighton <bryan.leigh77@gmail.com>
2020-12-31 17:24:23 -0800

    secret env for secrets

--- api/app/settings.py
+++ api/app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vmdxq_1(b(s++ehwnz=4ohd_^8-#30eh^7sohz!eak5jkdj^hk'
+SECRET_KEY = os.environ['DJANGO_APP_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
